2ea26b93e7cd828cca4d6b197405af5d83bda7ff
Hayden Riddiford <hayden@terrakaffe.com>
2024-10-31 15:25:27 -0700

    - Refactor for a much simpler cache

--- src/day12.zig
+++ src/day12.zig
@@ -63,100 +63,79 @@ test "parseLine" {
     try std.testing.expectEqualSlices(usize, &.{ 1, 1, 1, 1, 1 }, ret_folded.group_sizes.slice());
 }
 
-// Building our own hasher... YAY!
-//
-//
-//
-
 const CacheKey = struct {
-    springs: []const u8,
-    groups: []const usize,
+    spring_idx: usize,
+    group_idx: usize,
 };
 
-const CacheContext = struct {
-    pub fn hash(_: CacheContext, key: CacheKey) u64 {
-        var hasher = std.hash.Wyhash.init(0);
-        std.hash.autoHashStrat(&hasher, key, .Deep);
-        // hasher.update(key.springs);
-        // hasher.update(std.mem.sliceAsBytes(key.groups));
-        return hasher.final();
-    }
-
-    pub fn eql(_: CacheContext, a: CacheKey, b: CacheKey) bool {
-        return std.mem.eql(u8, a.springs, b.springs) and std.mem.eql(usize, a.groups, b.groups);
-    }
-};
-
-const Cache = std.HashMap(CacheKey, usize, CacheContext, std.hash_map.default_max_load_percentage);
+const Cache = std.AutoHashMap(CacheKey, usize);
 
 test "Cache HashMap" {
     var hash_map = Cache.init(std.testing.allocator);
     defer hash_map.deinit();
-    const dummy_key: CacheKey = .{ .springs = "..##..", .groups = &.{ 1, 2, 3 } };
+    const dummy_key: CacheKey = .{ .spring_idx = 0, .group_idx = 1 };
 
     try hash_map.put(dummy_key, 100);
     try std.testing.expectEqual(100, hash_map.get(dummy_key).?);
-    try std.testing.expectEqual(null, hash_map.get(.{ .springs = "...", .groups = &.{ 1, 2, 3 } }));
+    try std.testing.expectEqual(null, hash_map.get(.{ .spring_idx = 0, .group_idx = 0 }));
 }
 
-var cache: Cache = undefined;
-
-fn findPossibilities(springs: []const u8, groups: []const usize) usize {
+fn findPossibilitiesInternal(springs: []const u8, spring_idx: usize, groups: []const usize, group_idx: usize, cache: *Cache) usize {
 
     // End conditions:
-    if (groups.len == 0) {
+    if (group_idx >= groups.len) {
         // No more groups
-        if (springs.len == 0) {
+        if (spring_idx >= springs.len) {
             // Also no more springs, valid
             return 1;
         } else {
             // If more springs, but all '.' or '?', valid, else invalid
-            return if (std.mem.indexOfScalar(u8, springs, '#')) |_| 0 else 1;
+            return if (std.mem.indexOfScalar(u8, springs[spring_idx..], '#')) |_| 0 else 1;
         }
-    } else if (springs.len < groups[0]) {
+    } else if (springs[spring_idx..].len < groups[group_idx]) {
         // Less springs than required group, invalid
         return 0;
     }
 
-    if (springs[0] == '.') {
+    if (springs[spring_idx] == '.') {
         // Skip to next character
-        return findPossibilities(springs[1..], groups);
+        return findPossibilitiesInternal(springs, spring_idx + 1, groups, group_idx, cache);
     } else {
         const is_valid = v: {
             // Can't have any '.' within a group
-            if (std.mem.indexOfScalar(u8, springs[0..groups[0]], '.')) |_| break :v false;
+            if (std.mem.indexOfScalar(u8, springs[spring_idx .. spring_idx + groups[group_idx]], '.')) |_| break :v false;
 
             // Last character can not be '#'
-            if (springs.len > groups[0]) {
-                if (springs[groups[0]] == '#') {
+            if (springs[spring_idx..].len > groups[group_idx]) {
+                if (springs[spring_idx + groups[group_idx]] == '#') {
                     break :v false;
                 }
             } else {
                 // Special case where we exhausted springs so can immediately determine if this is a valid sequence
-                if (groups.len > 1) return 0 else return 1;
+                if ((groups.len - group_idx) > 1) return 0 else return 1;
             }
 
             break :v true;
         };
         var ret: usize = 0;
-        const key: CacheKey = .{ .springs = springs, .groups = groups };
+        const key: CacheKey = .{ .spring_idx = spring_idx, .group_idx = group_idx };
         if (cache.get(key)) |v| {
             ret = v;
         } else {
             if (is_valid) {
-                if (springs[0] == '?') {
+                if (springs[spring_idx] == '?') {
                     // Try both paths, ? is a # and thus valid group, or a . and skip to next char
-                    ret = findPossibilities(springs[1..], groups) + findPossibilities(springs[groups[0] + 1 ..], groups[1..]);
+                    ret = findPossibilitiesInternal(springs, spring_idx + 1, groups, group_idx, cache) + findPossibilitiesInternal(springs, spring_idx + groups[group_idx] + 1, groups, group_idx + 1, cache);
                 } else {
                     // Continue on since this group must go here
-                    ret = findPossibilities(springs[groups[0] + 1 ..], groups[1..]);
+                    ret = findPossibilitiesInternal(springs, spring_idx + groups[group_idx] + 1, groups, group_idx + 1, cache);
                 }
-            } else if (springs[0] == '#') {
+            } else if (springs[spring_idx] == '#') {
                 // If group is invalid but the first char is # then this combo can't be valid
                 ret = 0;
             } else {
                 // Still a chance this group could be valid, continue
-                ret = findPossibilities(springs[1..], groups);
+                ret = findPossibilitiesInternal(springs, spring_idx + 1, groups, group_idx, cache);
             }
             const v = cache.getOrPut(key) catch unreachable;
             if (!v.found_existing) {
@@ -167,37 +146,39 @@ fn findPossibilities(springs: []const u8, groups: []const usize) usize {
     }
 }
 
-test "findPossibilities - Normal" {
-    cache = Cache.init(std.testing.allocator);
+fn findPossibilities(springs: []const u8, groups: []const usize, allocator: std.mem.Allocator) usize {
+    var cache = Cache.init(allocator);
     defer cache.deinit();
-    try std.testing.expectEqual(1, findPossibilities("???.###", &.{ 1, 1, 3 }));
-    try std.testing.expectEqual(4, findPossibilities(".??..??...?##.", &.{ 1, 1, 3 }));
-    try std.testing.expectEqual(1, findPossibilities("?#?#?#?#?#?#?#?", &.{ 1, 3, 1, 6 }));
-    try std.testing.expectEqual(1, findPossibilities("????.#...#...", &.{ 4, 1, 1 }));
-    try std.testing.expectEqual(4, findPossibilities("????.######..#####.", &.{ 1, 6, 5 }));
-    try std.testing.expectEqual(10, findPossibilities("?###????????", &.{ 3, 2, 1 }));
+    return findPossibilitiesInternal(springs, 0, groups, 0, &cache);
+}
+
+test "findPossibilities - Normal" {
+    try std.testing.expectEqual(1, findPossibilities("???.###", &.{ 1, 1, 3 }, std.testing.allocator));
+    try std.testing.expectEqual(4, findPossibilities(".??..??...?##.", &.{ 1, 1, 3 }, std.testing.allocator));
+    try std.testing.expectEqual(1, findPossibilities("?#?#?#?#?#?#?#?", &.{ 1, 3, 1, 6 }, std.testing.allocator));
+    try std.testing.expectEqual(1, findPossibilities("????.#...#...", &.{ 4, 1, 1 }, std.testing.allocator));
+    try std.testing.expectEqual(4, findPossibilities("????.######..#####.", &.{ 1, 6, 5 }, std.testing.allocator));
+    try std.testing.expectEqual(10, findPossibilities("?###????????", &.{ 3, 2, 1 }, std.testing.allocator));
 }
 
 test "findPossibilities - Folded" {
-    cache = Cache.init(std.testing.allocator);
-    defer cache.deinit();
     var data = try parseLine("???.### 1,1,3", true);
-    try std.testing.expectEqual(1, findPossibilities(data.spring_str.slice(), data.group_sizes.slice()));
+    try std.testing.expectEqual(1, findPossibilities(data.spring_str.slice(), data.group_sizes.slice(), std.testing.allocator));
 
     data = try parseLine(".??..??...?##. 1,1,3", true);
-    try std.testing.expectEqual(16384, findPossibilities(data.spring_str.slice(), data.group_sizes.slice()));
+    try std.testing.expectEqual(16384, findPossibilities(data.spring_str.slice(), data.group_sizes.slice(), std.testing.allocator));
 
     data = try parseLine("?#?#?#?#?#?#?#? 1,3,1,6", true);
-    try std.testing.expectEqual(1, findPossibilities(data.spring_str.slice(), data.group_sizes.slice()));
+    try std.testing.expectEqual(1, findPossibilities(data.spring_str.slice(), data.group_sizes.slice(), std.testing.allocator));
 
     data = try parseLine("????.#...#... 4,1,1", true);
-    try std.testing.expectEqual(16, findPossibilities(data.spring_str.slice(), data.group_sizes.slice()));
+    try std.testing.expectEqual(16, findPossibilities(data.spring_str.slice(), data.group_sizes.slice(), std.testing.allocator));
 
     data = try parseLine("????.######..#####. 1,6,5", true);
-    try std.testing.expectEqual(2500, findPossibilities(data.spring_str.slice(), data.group_sizes.slice()));
+    try std.testing.expectEqual(2500, findPossibilities(data.spring_str.slice(), data.group_sizes.slice(), std.testing.allocator));
 
     data = try parseLine("?###???????? 3,2,1", true);
-    try std.testing.expectEqual(506250, findPossibilities(data.spring_str.slice(), data.group_sizes.slice()));
+    try std.testing.expectEqual(506250, findPossibilities(data.spring_str.slice(), data.group_sizes.slice(), std.testing.allocator));
 }
 
 fn calculateAnswer(allocator: std.mem.Allocator) ![2]usize {
@@ -207,12 +188,10 @@ fn calculateAnswer(allocator: std.mem.Allocator) ![2]usize {
     var file_line_reader = try helpers.lineReaderFromAdventDay(12, allocator);
     defer file_line_reader.deinit();
     while (file_line_reader.next()) |line| {
-        cache = Cache.init(allocator);
-        defer cache.deinit();
         const spring_data = try parseLine(line, false);
-        answer1 += findPossibilities(spring_data.spring_str.slice(), spring_data.group_sizes.slice());
+        answer1 += findPossibilities(spring_data.spring_str.slice(), spring_data.group_sizes.slice(), allocator);
         const spring_data_folded = try parseLine(line, true);
-        answer2 += findPossibilities(spring_data_folded.spring_str.slice(), spring_data_folded.group_sizes.slice());
+        answer2 += findPossibilities(spring_data_folded.spring_str.slice(), spring_data_folded.group_sizes.slice(), allocator);
         answer2 += 0;
     }
 
